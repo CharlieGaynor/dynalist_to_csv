3dab7adccef32727b78b14a983f47ab3739c04e0
Charlie <charliejackgaynor@gmail.com>
2022-07-18 08:58:32 +0100

    Adding token template

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
-TOKEN = 'TR_BhEfcQfwh6bWj-CoRgxWEJkgcV8ItWneBlNVn-cpl0gY0-U2WlyRSb8PM17_dDuYRg4djxpg8J-mCL_mgYt48EwSnNGG60tOStqJc1-QsE2k0C_c0wyXq2bakHhf5'
-
+TOKEN = "your_token_goes_here"
 DELIMITER = ","
 DELIMITER_REPLACEMENT = ","
 REVERT_COLOR = 'red'  # The keys in the below dict are the possible values for here
